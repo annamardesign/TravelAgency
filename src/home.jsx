f9ec12a33bc74a3285dbf2564b468f7c65994202
Anna Marinova <lalogica@gmail.com>
2020-12-22 17:15:43 +0200

    Cards Component Added

--- src/home.jsx
+++ src/home.jsx
@@ -2,30 +2,52 @@ import React, { useEffect, useState } from 'react';
 import sky from './images/sky.png';
 import mountain from './images/backmountain.png';
 import water from './images/water.png';
+import { offersData } from './services/offersData.js';
+import { BsChevronDoubleDown } from 'react-icons/bs';
 import './home.css';
 
 const Home = () => {
 
   const [offset, setOffset] = useState(0);
+  const [offers, setOffers] = useState([]);
 
   useEffect(() => {
     function handleScroll() {
       setOffset(window.pageYOffset)
     }
     window.addEventListener("scroll", handleScroll)
+
+    function loadData() {
+      setOffers(offersData);
+    }
     return () => {
       window.removeEventListener("scroll", handleScroll)
     }
   }, [])
 
     return (<div className="page">
-        <div className="sky"><img src={sky} /></div>
-        <div className="mountain"><img src={mountain}/></div>
-        <div className="mountain"><img src={mountain} style={{
+        <div className="sky"><img src={sky} alt="sky"/></div>
+        <div className="mountain"><img src={mountain} alt="mountain"/></div>
+        <div className="mountain"><img src={mountain} alt="mountain" style={{
         transform: `translateY(${offset * 0.5}px)`}} /></div>
-        <div className="water"><img src={water}/><h1>Explore</h1></div>
-        <div className="find"><h1>Find your dream destination</h1></div>
-    </div>  );
+        <div className="water">
+            <img src={water} alt="water"/>
+            <h1 className="chamonie" style={{transform: `translateX(${offset * 1}px)`}}>
+                Like what you see? Explore Chamonie, France!</h1>
+        </div>
+        <div className="find"><h1>Find your dream destination</h1><BsChevronDoubleDown className="find-arrow"/></div>
+        <h1 className="top" style={{transform: `translateX(${offset * -0.5}px)`}}> Top Destinations </h1>
+        <div className="grid-wrap">
+          {offersData.map( offer => 
+            (<div className="card">
+              <img src={offer.image}  alt={offer.description} className="offer"/> 
+              <h5 className="price">{offer.price}</h5>
+              <p className="desc">{offer.description}</p>
+              <a href={offer.url} className="btn-see">See itinerary</a>
+            </div>)
+            )}
+        </div>
+    </div>);
 }
  
 export default Home;
---
+++ src/services/offersData.js
@@ -0,0 +1,36 @@
+
+
+export const offersData = [
+    {
+      url: "",
+      image: require("../images/sky.png"),
+      price: "620 USD",
+      id: "1",
+      description: "Japan"
+    },
+
+    {
+     url: "",
+     image: require("../images/croatia.jpg"),
+     price: "199 USD",
+     id: "2",
+     description: "Croatia"
+      
+    },
+    {
+      url: "",
+      image: require("../images/maldives.jpg"),
+      price: "1099 USD",
+      id: "3",
+      description: "Maldives"
+      },
+      {
+      url: "",
+      image: require("../images/riviera.jpg"),
+      price: "499 USD",
+      id: "4",
+      description: "Riviera"
+      }
+];
+
+  
