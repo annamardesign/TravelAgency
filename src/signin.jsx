5483c72166b823ea0fc87b0017683453a54b4db1
Anna Marinova <lalogica@gmail.com>
2020-12-23 19:49:41 +0200

    Sign up css mobile

--- src/signin.jsx
+++ src/signin.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 
 const SignIn = () => {
-    return ( <div>Sign in</div> );
+    return ( <React.Fragment>
+        <div className="email">
+            <div className="email-container">
+                <h1>Get members-only discounts</h1>
+                <form action="#">
+                    <div className="form-wrap">
+                        <label for="email">
+                            <input type="email" placeholder="Enter your email" id="email"></input>
+                        </label>
+                        <button className="button" type="submit">Sign up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </React.Fragment> );
 }
  
 export default SignIn;
